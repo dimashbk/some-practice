81d2d8579e6d86970baacd101fa483752c9b9eb3
dimash <200103433@stu.sdu.edu.kz>
2023-01-19 03:03:13 +0600

    Creating UICollectionView and Cell

---
+++ SomePractice/MyCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyCollectionViewCell.swift
+//  SomePractice
+//
+//  Created by Dinmukhamed on 19.01.2023.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
--- SomePractice/ViewController.swift
+++ SomePractice/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let myCollection = UICollectionView()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
     }
 
 
+}
+
+extension ViewController: UICollectionViewDelegate,UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let itemCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? MyCollectionViewCell {
+            return itemCell
+        }
+        return UICollectionViewCell()
+    }
+    
+    
+    private func initialize(){
+        
+        myCollection.delegate = self
+        myCollection.dataSource = self
+        view.addSubview(myCollection)
+        
+        
+    }
+    
+    
 }
 
