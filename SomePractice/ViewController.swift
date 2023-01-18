1a7c638ec3bc2514d995c981c7126bf5fff6dffa
dimash <200103433@stu.sdu.edu.kz>
2023-01-19 02:43:02 +0600

    Removing Storyboard

---
+++ SomePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SomePractice
+//
+//  Created by Dinmukhamed on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
